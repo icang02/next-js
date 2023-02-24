68f2e9b348545a52b00be53349795e66fa5d8d96
icang02 <ilmifaizan1112@gmail.com>
2023-02-24 22:29:13 +0800

    updaed

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NextNProgress from "nextjs-progressbar";
+import NextNProgress from "nextjs-progressbar"
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  
   return (
     <>
-      <NextNProgress />
+      <NextNProgress color="#29D" startPosition={0.3} stopDelayMs={0} height={3} showOnShallow={false} />
       <Component {...pageProps} />;
     </>
   );
